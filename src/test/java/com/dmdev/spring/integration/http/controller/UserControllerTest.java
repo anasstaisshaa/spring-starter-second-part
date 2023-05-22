eee320fba17d7eb4d4c5194396116a724fb27b04
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-05-22 23:23:21 +0300

    add tests for UserController

---
+++ src/test/java/com/dmdev/spring/integration/http/controller/UserControllerTest.java
@@ -0,0 +1,49 @@
+package com.dmdev.spring.integration.http.controller;
+
+import com.dmdev.spring.integration.annotation.IT;
+import lombok.RequiredArgsConstructor;
+import org.hamcrest.collection.IsCollectionWithSize;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.collection.IsCollectionWithSize.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@IT
+@AutoConfigureMockMvc
+@RequiredArgsConstructor
+class UserControllerTest {
+
+    private final MockMvc mockMvc;
+
+    @Test
+    void findAll() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("user/users"))
+                .andExpect(model().attributeExists("users"))
+                .andExpect(model().attribute("users", hasSize(5)));
+    }
+
+    @Test
+    void create() throws Exception {
+        mockMvc.perform(post("/users")
+                .param("username", "test@gmail.com")
+                .param("firstname", "Test")
+                .param("lastname", "TestTest")
+                .param("role", "ADMIN")
+                .param("companyId", "1")
+        )
+                .andExpectAll(
+                        status().is3xxRedirection(),
+                        redirectedUrlPattern("/users/*")
+                );
+    }
+
+
+}
