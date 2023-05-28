34b2da4fbb6498be175c091f4687133ec50eb838
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-05-28 18:09:55 +0300

    add HTTP Basic Authentication

--- src/main/java/com/dmdev/spring/config/SecurityConfiguration.java
+++ src/main/java/com/dmdev/spring/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.dmdev.spring.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,6 +14,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
+//                .httpBasic(Customizer.withDefaults());
                 .formLogin(login -> login
                         .loginPage("/login")
                         .defaultSuccessUrl("/users")
